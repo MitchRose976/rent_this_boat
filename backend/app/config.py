48344750f3b287d388e6cae923cfa5ea7c270c7b
MitchRose976 <mitch.j.rose@outlook.com>
2025-09-27 18:09:12 -0400

    fix: pymongo

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "Awesome API"
+    app_name: str = "Rent This Boat API"
     admin_email: str
     items_per_user: int = 50
-    mongo_uri: str = ""
+    mongo_uri: str
 
     class Config:
         env_file = ".env"  # Path to your .env file
